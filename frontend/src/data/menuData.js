48a8920ca9bd96b2da76acca04860c713ef96f3e
denylie <3592165+denylie@users.noreply.github.com>
2020-02-25 14:38:18 -0800

    created sample data for Vendor menu

---
+++ frontend/src/data/menuData.js
@@ -0,0 +1,31 @@
+const menuData = [
+   {
+       id: 1,
+       pic: "../assets/m1.jpg",
+       name: "Space Needle Dog",
+       desc: "You will be flying high",
+       price: 6.99,
+       available: true
+
+   },
+   {
+        id: 2,
+        pic: "AD320-power-team/frontend/src/assets/m1.jpg",
+        name: "G-Dog",
+        desc: "You will not go any other way",
+        price: 6.99,
+        available: true
+
+    },
+    {
+        id: 3,
+        pic: "../assets/m1.jpg",
+
+        name: "Spicy Yellow Weiner",
+        desc: "It has a little kick",
+        price: 6.99,
+        available: false
+    }
+]
+
+export default menuData;
