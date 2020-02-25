76e83679e8e7224b744328b3762efc7f7dbc016a
denylie <3592165+denylie@users.noreply.github.com>
2020-02-25 14:40:28 -0800

    Created Vendor Page

--- frontend/src/vendor/MenuItem.js
+++ frontend/src/vendor/MenuItem.js
@@ -1,13 +1,41 @@
 import React from 'react';
 import './MenuItem.css';
-import menuPic from '../assets/menu-pic.png'
+import samplePic from '../assets/m1.jpg';
 
-function MenuItem() {
+function MenuItem(props) {
    return(
       <div className="Menu-Item">
-         <input type="checkbox" />
-         <p>Placeholder menu item</p>
-         <img src={menuPic}/>
+         <table className="Table">
+            <tr>
+               <td className ="td1"><input type="checkbox" defaultChecked={props.hotdog.available}/></td>
+               <td className ="td2">
+                  {/* ================ Name ============ */}
+                     <h3 style={{textDecoration: !props.hotdog.available && "line-through", color: !props.hotdog.available && "#808080"}}>
+                        {props.hotdog.name}
+                     </h3>
+                  {/* ================ desc ============ */}
+                     <p style={{textDecoration: !props.hotdog.available && "line-through",color: !props.hotdog.available && "#808080"}}>
+                        {props.hotdog.desc}
+                     </p>   
+                  {/* ================ price ============ */}
+                     <p style={{textDecoration: !props.hotdog.available && "line-through",color: !props.hotdog.available && "#808080"}}>
+                        {props.hotdog.price.toLocaleString("en-US", { style: "currency", currency: "USD" })}
+                     </p> 
+               </td>
+               <td className = "td3">
+                  {/* ================ picture ============ */}
+                     <img src={samplePic} alt="menuPic"/>   
+               </td>
+            </tr>
+         </table>
+            
+
+         
+            
+         
+         
+         
+
       </div>
    );
 }
--- frontend/src/vendor/Vendor.js
+++ frontend/src/vendor/Vendor.js
@@ -1,18 +1,39 @@
 import React from 'react';
 import './Vendor.css';
 import MenuItem from './MenuItem';
+import menuData from "../data/menuData"
 
 import logo from '../assets/hotdogcart.png'
 
+
+
 function Vendor () {
+   //Arrow fucntion version
+  //jokesData.map(joke => <Joke question={joke.question} punchLine={joke.punchLine} />)
+
+                        // mapping essentially extracting json data and
+                        // place it in the array
+  const menuItemComponents = menuData.map( 
+      (hotdog) => {
+         return(<MenuItem
+                  key={hotdog.id} //always needed else there will be console log error.. has to be unique
+                  hotdog = {hotdog} 
+               />
+         )
+      }
+   ) 
    return(
       //vendor will show the paper list
-      <div className="Vendor"> 
-         <img src={logo} alt="logo" />
-         <h1>Menu</h1>
-         <MenuItem />
-         <MenuItem />
-         <MenuItem />
+      <div className = "Bg">
+         <div className="Vendor"> 
+            <header className="V-header">
+               <img src={logo} alt="logo" />
+               <h1>Menu</h1>
+            </header>
+            
+            
+            {menuItemComponents}         
+         </div>
       </div>
    )
 }
