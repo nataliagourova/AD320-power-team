9ff85e9db1f62f94cd28322764ffff56f8f012db
Gourova <Natalia.Gourova1@T-Mobile.com>
2020-02-25 21:19:23 -0800

    first commit

---
+++ frontend/src/vendor/VendorOrders.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import './VendorOrders.css';
+import MenuItem from './MenuItem';
+import menuData from "../data/menuData"
+
+import logo from '../assets/hotdogcart.png'
+
+
+
+function VendorOrders () {
+   //Arrow fucntion version
+  //jokesData.map(joke => <Joke question={joke.question} punchLine={joke.punchLine} />)
+
+                        // mapping essentially extracting json data and
+                        // place it in the array
+  const menuItemComponents = menuData.map( 
+      (hotdog) => {
+         return(<MenuItem
+                  key={hotdog.id} //always needed else there will be console log error.. has to be unique
+                  hotdog = {hotdog} 
+               />
+         )
+      }
+   ) 
+   return(
+      //vendor will show the paper list
+      <div className = "Bg">
+         <div className="Vendor"> 
+            <header className="V-header">
+               <img src={logo} alt="logo" />
+               <h1>Orders</h1>
+            </header>
+            
+            
+            {menuItemComponents}         
+         </div>
+      </div>
+   )
+}
+
+export default VendorOrders;
+
+
+
+
