68cc234ac07eac63f73dcd2aec31b8fe104d6c0a
Gourova <Natalia.Gourova1@T-Mobile.com>
2020-02-25 21:13:10 -0800

    initial commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './assets/hotdogcart.png';
+import VendorOrders from './vendor/VendorOrders';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+            {/* <header className="App-header">
+        <h3>Welcome to Wheelie Wiener</h3>
+        <img src={logo} className="App-logo" alt="logo" />
+        <input type="button" className="App-button" value="Admin"></input>
+        <input type="button" className="App-button" value="Vendor"  ></input>
+        <input type="button" className="App-button" value="Customer"  ></input>
+      </header> */}
+      <VendorOrders />;
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
