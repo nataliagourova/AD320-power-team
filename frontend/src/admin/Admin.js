459b27ae714c570bdb46de83ac845969c3c63c9d
denylie <3592165+denylie@users.noreply.github.com>
2020-02-27 14:32:03 -0800

    initialize admin folder n file

---
+++ frontend/src/admin/Admin.js
@@ -0,0 +1,20 @@
+import React from 'react';
+// import './Admin.css';
+
+
+function Admin () {
+    
+   return(
+      <div>
+         <header>
+            This is ADMIN PAGE
+         </header>
+      </div>
+   )
+}
+
+export default Admin;
+
+
+
+
