aecfdbddd3648fbbb99d4e4371911ec43be3b99d
denylie <3592165+denylie@users.noreply.github.com>
2020-03-12 00:14:35 -0700

    Added description

--- frontend/src/data/vendorData.js
+++ frontend/src/data/vendorData.js
@@ -2,12 +2,14 @@ const vendor = [
    {
       cartId : 1,
       cartName: "Talisker",
+      description: "asdsadasdas sadas dasd asdasdsa da",
       cartActive: true,
       location: [47.708222, -122.326861]
    },
    {
       cartId : 2,
       cartName: "Lagavulin",
+      description: "asdsadasdas sadas dasd asdasdsa da",
       cartActive: true,
       location:[47.711799, -122.332392]
 
