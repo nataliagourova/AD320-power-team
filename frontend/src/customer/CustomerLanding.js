fe9ba5f7e44d0e5a9c47ad58c3032fe513872225
denylie <3592165+denylie@users.noreply.github.com>
2020-03-12 12:23:49 -0700

    tweaked nav bar inside customerLanding page

--- frontend/src/customer/CustomerLanding.js
+++ frontend/src/customer/CustomerLanding.js
@@ -12,7 +12,7 @@
 
    //--- BootStrap import
       import 'bootstrap/dist/css/bootstrap.min.css';
-      import { Button,Container,Row,Col,Navbar, Nav } from 'react-bootstrap';
+      import { Button,Container,Row,Col,Navbar, Nav, Form, FormControl } from 'react-bootstrap';
 
    //--- Google-maps-react import
       import GMap from './components/GoogleMaps';
@@ -22,27 +22,46 @@
 class CustomerLanding extends React.Component {
    render(){
       return(
-         // <div style={ styleCustLanding }>
          <Container>
-            <div className="custHome">
-                           
-               <header> 
-                  <Navbar>
-                     <img src={Logo} style={{width: '5%'}} alt = "Logo"/>
-                     <Nav>
-                        Search
-                     </Nav>
-                     <Link className="pic" to = "/customer/search">
-                        Search Bar
-                     </Link>
+            <div className="custHome">  
+
+                  <Navbar collapseOnSelect expand="md" bg="light" variant="light" collapseOnSelect expand="lg">
+                  
+                     <Navbar.Brand  href="/">
+                        <img src={Logo} 
+                             width='50' 
+                             alt = "Logo"
+                             className="d-inline-block align-top"></img>
+                        <h5 className="wwText">Wheelie Wiener</h5>
+                     </Navbar.Brand>
+
+                     <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
+
+                     <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="mr-auto">
+                           <Nav.Link href="/">                    Home  </Nav.Link>
+                           <Nav.Link href="/customer/menuOrder">  Order </Nav.Link>
+                           <Nav.Link href="/customer/menuOrder">  Menu  </Nav.Link>
+                        </Nav>
+
+                        <Form inline>
+                        <Nav.Link href="/">                    Home  </Nav.Link>
+
+                           <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                           <Button variant="outline-primary" >Search</Button>
+                        </Form>
+                     </Navbar.Collapse>
+                     
+
                   </Navbar>
+
                   <GMap />
 
                   <Row>
                      <Col> Column 1 of 2</Col>
                      <Col> Column 2 of 2</Col>
                   </Row>
-               </header>
+               {/* </header> */}
 
             </div>
             
