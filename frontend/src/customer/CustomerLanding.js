e33a4658ffbe6b070cd032192c710ab5f4ae8688
denylie <3592165+denylie@users.noreply.github.com>
2020-03-12 13:53:04 -0700

    extracted out NavBar to create customerNav componenst

--- frontend/src/customer/CustomerLanding.js
+++ frontend/src/customer/CustomerLanding.js
@@ -7,9 +7,8 @@
    //--- CSS
       import './Customer.css';
 
-   //--- Image import
-      import Logo from '../assets/hotdogcart.png';
-
+   //--- Component import
+      import CustNav from './components/CustomerNav.js';
    //--- BootStrap import
       import 'bootstrap/dist/css/bootstrap.min.css';
       import { Button,Container,Row,Col,Navbar, Nav, Form, FormControl } from 'react-bootstrap';
@@ -24,45 +23,13 @@ class CustomerLanding extends React.Component {
       return(
          <Container>
             <div className="custHome">  
-
-                  <Navbar collapseOnSelect expand="md" bg="light" variant="light" collapseOnSelect expand="lg">
-                  
-                     <Navbar.Brand  href="/">
-                        <img src={Logo} 
-                             width='50' 
-                             alt = "Logo"
-                             className="d-inline-block align-top"></img>
-                        <h5 className="wwText">Wheelie Wiener</h5>
-                     </Navbar.Brand>
-
-                     <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
-
-                     <Navbar.Collapse id="responsive-navbar-nav">
-                        <Nav className="mr-auto">
-                           <Nav.Link href="/">                    Home  </Nav.Link>
-                           <Nav.Link href="/customer/menuOrder">  Order </Nav.Link>
-                           <Nav.Link href="/customer/menuOrder">  Menu  </Nav.Link>
-                        </Nav>
-
-                        <Form inline>
-                           <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                           <Button variant="outline-primary" >Search</Button>
-                        </Form>
-                     </Navbar.Collapse>
-                     
-
-                  </Navbar>
-
+                  <CustNav/>
                   <GMap />
-
                   <Row>
                      <Col> Column 1 of 2</Col>
                      <Col> Column 2 of 2</Col>
                   </Row>
-               {/* </header> */}
-
             </div>
-            
          </Container>
       )
    }
