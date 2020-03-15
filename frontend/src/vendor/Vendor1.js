2ce0b2a5fb65d333af68c3a5c5796b50d25f60ee
Gourova <Natalia.Gourova1@T-Mobile.com>
2020-03-14 22:03:19 -0700

    add vendor info

--- frontend/src/vendor/Vendor1.js
+++ frontend/src/vendor/Vendor1.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import logo from '../assets/hotdogcart.png';
 import VendorOrders from './VendorOrders';
+import VendorInfo from './VendorInfo';
 import BottomMenu from './BottomMenu';
 import './Vendor1.css';
 
 function Vendor1() {
   return (
     <div className="App1">
-
-      <VendorOrders />;
+      <VendorOrders vendorCartId={1}/>;
 	    <BottomMenu />
     </div>
   );
--- frontend/src/vendor/VendorOrders.js
+++ frontend/src/vendor/VendorOrders.js
@@ -1,41 +1,45 @@
 import React from 'react';
 import './VendorOrders.css';
 import MenuItem1 from './MenuItem1';
+import VendorInfo from './VendorInfo';
 import menuData1 from "../data/menuData1.js"
 
 import logo from '../assets/hotdogcart.png'
 
 
 
-function VendorOrders () {
-   //Arrow function version
-  //jokesData.map(joke => <Joke question={joke.question} punchLine={joke.punchLine} />)
+class VendorOrders extends React.Component {
+   render() {
+         //Arrow function version
+      //jokesData.map(joke => <Joke question={joke.question} punchLine={joke.punchLine} />)
 
-                        // mapping essentially extracting json data and
-                        // place it in the array
-  const menuItemComponents = menuData1.map( 
-      (hotdog) => {
-         return(<MenuItem1
-                  key={hotdog.id} //always needed, otherwise there will be a console log error,it  has to be unique
-                  hotdog = {hotdog} 
-               />
-         )
-      }
-   ) 
-   return(
-      //vendor will show the paper list
-      <div className = "VendorOrder-Bg2">
-         <div className="Vendor2"> 
-            <header className="V-header2">
-               <img src={logo} alt="logo" />
-               <h1>New Orders</h1>
-            </header>
-            
-            
-            {menuItemComponents}         
-         </div>
-      </div>
-   )
+                              // mapping essentially extracting json data and
+                              // place it in the array
+      const menuItemComponents = menuData1.map( 
+            (hotdog) => {
+               return(<MenuItem1
+                        key={hotdog.id} //always needed, otherwise there will be a console log error,it  has to be unique
+                        hotdog = {hotdog} 
+                     />
+               )
+            }
+         ) 
+         return(
+            //vendor will show the paper list
+            <div className = "VendorOrder-Bg2">
+               <div className="Vendor2"> 
+                  <header className="V-header2">
+                     <img src={logo} alt="logo" />
+                     <h1>New Orders</h1>
+                     <VendorInfo vendorCartId={this.props.vendorCartId} />
+                  </header>
+                  
+                  
+                  {menuItemComponents}         
+               </div>
+            </div>
+         );
+   }
 }
 
 export default VendorOrders;
