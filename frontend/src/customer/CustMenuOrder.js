379b6d30253a8b8dcb1826bb9acc3951c68e18d2
denylie <3592165+denylie@users.noreply.github.com>
2020-03-19 10:01:05 -0700

    added menu item into order page

--- frontend/src/customer/CustMenuOrder.js
+++ frontend/src/customer/CustMenuOrder.js
@@ -6,19 +6,35 @@
 
    //--- Component import
       import CustNav from './components/CustomerNav.js';
+      import menuData from '../data/menuData';
+      import MenuItem from '../vendor/MenuItem'
+
    //--- BootStrap import
       import 'bootstrap/dist/css/bootstrap.min.css';
       import { Container,Row,Col} from 'react-bootstrap';
 //===================================================================
 
 class CustMenuOrder extends Component {
+   
    render(){
+
+      const menuItemComponents = menuData.map( 
+         (hotdog) => {
+            return(<MenuItem
+                     key={hotdog.id} //always needed else there will be console log error.. has to be unique
+                     hotdog = {hotdog} 
+                  />
+            )
+         }
+      )
       return(
          <div className="custMenuOrder">
             <Container>
                <CustNav/>
                <h1>Menu or Order Page</h1>
+               {menuItemComponents}
             </Container>
+
          </div>
 
       )
